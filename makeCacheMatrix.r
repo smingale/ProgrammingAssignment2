fea0f552a8caa9214cb1654d024477620636b533
smingale <sanjay.ingale@hotmail.com>
2016-02-06 19:07:08 +0530

    Update and rename makeCacheMatrix to makeCacheMatrix.r

--- makeCacheMatrix.r
+++ makeCacheMatrix.r
@@ -15,5 +15,5 @@ makeCacheMatrix <- function(x = matrix()) {
         get = function() x
         setinv = function(inverse) inv <<- inverse 
         getinv = function() inv
-        list(set=set, get=get, setinv=setinv, getinv=getinv)
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
